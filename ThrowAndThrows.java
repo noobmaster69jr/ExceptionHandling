bf4e58a7f205bf994ba82310a0e575b21544cc2e
noobmaster69jr <srashwin22@gmail.com>
2023-09-13 15:31:29 +0530

    throw and throws

---
+++ ThrowAndThrows.java
@@ -0,0 +1,15 @@
+package Exception;
+
+public class ThrowAndThrows {
+	public static void main(String[] args) {
+		System.out.print(divide(10, 0));
+	}
+
+	public static double divide(int a, int b) throws RuntimeException {
+		if (b == 0) {
+			throw new IllegalArgumentException("Cannot divide by 0");
+		}
+		return a / b;
+	}
+
+}
